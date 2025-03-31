19787c70298f5c6a7fbb30e10191c4bc1376d39b
cjh-19 <gns8812@naver.com>
2025-03-31 12:52:20 +0900

    [Fix #15] 프로필 조회시 file path 수정 (#15)

--- jobbotdari_user/src/main/java/com/team9/jobbotdari/service/ProfileService.java
+++ jobbotdari_user/src/main/java/com/team9/jobbotdari/service/ProfileService.java
@@ -35,8 +35,7 @@ public class ProfileService {
 
         File file = fileRepository.findTopByUserIdOrderByCreatedAtDesc(user.getId()).orElse(null);
 
-//        String fileUrl = (file != null) ? fileAccessUrl + file.getFilename() : null;
-        String fileUrl = (file != null) ? fileService.getFileUrl(file.getFilePath()) : null;
+        String fileUrl = (file != null) ? fileAccessUrl + file.getFilename() : null;
 
         return new ProfileResponseDto(
                 user.getId(),
