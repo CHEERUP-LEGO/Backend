00354c1feccb67e5437f6eb66d1b357a75b97899
Jihoony_.c <66457014+cjh-19@users.noreply.github.com>
2025-04-01 17:54:16 +0900

    [Feat #29] ec2 ip 주소 적용 (#29) (#30)

--- jobbotdari_user/src/main/java/com/team9/jobbotdari/kafka/messagequeue/KafkaConsumerConfig.java
+++ jobbotdari_user/src/main/java/com/team9/jobbotdari/kafka/messagequeue/KafkaConsumerConfig.java
@@ -18,7 +18,7 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker-9092-dns:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.143.42.91:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "log-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
