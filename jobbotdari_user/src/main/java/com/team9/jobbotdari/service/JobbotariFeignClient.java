89e4b0ddde53530b1a79697ba6ebcb8e8bc75dea
cjh-19 <gns8812@naver.com>
2025-04-02 04:09:55 +0900

    [Fix #35] feign hostname 수정 (#35)

--- jobbotdari_user/src/main/java/com/team9/jobbotdari/service/JobbotariFeignClient.java
+++ jobbotdari_user/src/main/java/com/team9/jobbotdari/service/JobbotariFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("jobbotdari")
+@FeignClient("jobbotdari-8081-dns")
 public interface JobbotariFeignClient {
     @RequestMapping(method = RequestMethod.POST, value = "/api/company", consumes = "application/json")
     BaseResponseDto addCompany(@RequestBody AddCompanyRequestDto addCompanyRequestDto);
